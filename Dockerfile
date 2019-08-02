d75477c2788332a3d30dead69f9f1191de769221
markmcgookin <mark.mcgookin@gmail.com>
2019-08-02 18:13:51 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS build
+
+# Set app as the working directory
+WORKDIR /app
+
+# copy csproj and restore as distinct layers
+COPY *.csproj .
+
+#RUN ["dotnet", "restore", "--configfile", "nuget.config"]
+RUN ["dotnet", "restore"]
+
+# copy and publish app and libraries
+COPY *.* ./
+RUN dotnet publish -c Release -o out
+
+# Change back and remove user-secrets once this is figured out and tied up to keyvalue properly
+FROM mcr.microsoft.com/dotnet/core/runtime:2.2 AS runtime
+# FROM mcr.microsoft.com/dotnet/core/sdk:2.2 AS runtime
+
+# Copy user secrets across
+# COPY secrets.json /root/.microsoft/usersecrets/6724c2f3-2018-4e57-9aee-303d3ab984a4/
+
+# copy the library
+RUN ["apt-get", "update"]
+RUN ["apt-get", "-y", "install", "libgdiplus"]
+
+WORKDIR /app
+COPY --from=build /app/out ./
+ENTRYPOINT ["dotnet", "status-monitor.dll"]
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace status_monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            var ip = host.AddressList.FirstOrDefault(x => x.AddressFamily == AddressFamily.InterNetwork);
+            var cpu_bash = "top -bn1 | grep load | awk '{printf \"%.2f\", $(NF-2)}'";
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                cpu_bash = "top -l 1 -n 1 | grep Load | awk '{printf \"%.2f\", $(NF-2)}'";
+            }
+            var cpu = Bash(cpu_bash);
+
+            var mem_bash = "free -m | awk 'NR==2{printf \"%s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'";
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                mem_bash = "top -l 1 -n 1 | grep Load | awk '{printf \"%.2f\", $(NF-2)}'";
+            }
+
+            var mem = Bash(mem_bash);
+
+            Console.WriteLine(ip.ToString());
+            Console.WriteLine(cpu.ToString());
+            Console.WriteLine(mem.ToString());
+        }
+
+        public static string Bash(string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+            
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
